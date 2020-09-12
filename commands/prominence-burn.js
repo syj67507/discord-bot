9ddfaf929a63b78b25cbece2a5e8a6a668182a2d
Shams Jamal <syj67507@uga.edu>
2020-09-11 20:32:54 -0400

    Command plays audio file and bot leaves when finished

--- commands/prominence-burn.js
+++ commands/prominence-burn.js
@@ -2,6 +2,29 @@ module.exports = {
     name: 'prominence-burn',
     description: 'Prominence Burn!!!',
     execute(message, args) {
-        message.channel.send('prominence burn');
+        const ytdl = require('ytdl-core');
+
+        // Validation checks
+        if (message.member.voice.channel == null) {
+            message.channel.send('You must join a voice channel.');
+            return;
+        }
+
+        // Let the bot join the channel
+        message.member.voice.channel.join()
+            .then((connection) => {
+                // Start yelling on successful join
+                console.log('Joined the channel');
+                const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=vaGzR9E9mPU'));
+
+                // Use dispatcher to identify the end of the clip
+                dispatcher.on('speaking', (speaking) => {
+                    if (!speaking) {
+                        message.member.voice.channel.leave();
+                    }
+                });
+            });
+        // catching errors will be handled in index.js
+
     },
 };
