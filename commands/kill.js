36d8e4b32d8d2b2b7e7baf5cf2f411671670d587
Shams Jamal <syj67507@uga.edu>
2020-09-27 13:29:28 -0400

    updated kill command files with error handling

--- commands/kill.js
+++ commands/kill.js
@@ -1,20 +1,23 @@
-"use strict";
+const UsageError = require('../custom_errors/usage_error');
+
 module.exports = {
     name: 'kill',
     description: 'Ay! Cammm onnn dooooood...',
-    execute(message, args) {
-        const client = message.client; // used for interval management
+    async execute(message, args) {
+
+        // used for interval management
+        const client = message.client;
 
         // validates user to be killed
-        if (args.length != 2 ) {
+        if (args.length != 2) {
             message.channel.send([
                 'UsageError: kill <@> <seconds>',
-                '10 seconds is the lowest you can set for <seconds>'
+                '60 seconds is the lowest you can set for <seconds>',
             ]);
-            return;
+            throw new UsageError('<seconds> argument not defined');
         }
-        if (args[1] < 10) {
-            args[1] = 10
+        if (args[1] < 60) {
+            args[1] = 60;
         }
         if (message.mentions.members.size == 0) {
             message.channel.send(`I can't find ${args[0]}, maybe he's already dead...`);
@@ -26,22 +29,21 @@ module.exports = {
         }
 
         // finds user to kill and sets up reiterating interval
-        message.channel.send(`Mr. Zurkon is here to kill: ${args[0]} :dagger:`);
-        message.mentions.members.first().fetch()
-            .then((guildMember) => {
-                const interval = client.setInterval(
-                    client.intervals.get('Kill Interval').execute,
-                    1000*args[1], // time
-                    message,
-                    args[0], // mention used for error handling
-                    client
-                );
-                console.log(args);
-                client.activeIntervals.set(args[0], interval);
-                console.log(client.activeIntervals);
-            })
-            .catch((error) => {
-                message.channel.send(`Error: ${error}`);
-            });
-    }
-}
+        await message.channel.send(`Mr. Zurkon is here to kill: ${args[0]} :dagger:`);
+        const guildMember = await message.mentions.members.first().fetch();
+        if (`<@!${guildMember.user.id}>` != args[0]) {
+            throw ReferenceError('Fetching guild member does not match user mention');
+        }
+        const interval = client.setInterval(
+            client.intervals.get('Kill Interval').execute,
+            1000 * args[1], // time
+            message,
+            args[0], // mention used for error handling
+            client,
+        );
+        console.log(args);
+        client.activeIntervals.set(args[0], interval);
+        console.log(client.activeIntervals);
+
+    },
+};
--- commands/revive.js
+++ commands/revive.js
@@ -1,15 +1,16 @@
-"use strict";
+const UsageError = require('../custom_errors/usage_error');
+
 module.exports = {
     name: 'revive',
     description: 'Ay! Cammm onnn dooooood... back in the game!!',
     execute(message, args) {
         if (args.length != 1) {
             console.log('UsageError: revive expects exactly 1 argument.');
-            return;
+            throw new UsageError('User mention not specified');
         }
         const client = message.client;
         if (!client.activeIntervals.has(args[0])) {
-            message.channel.send(`Can't revive a member who isn't being killed.`);
+            message.channel.send('Can\'t revive a member who isn\'t being killed.');
             return;
         }
 
@@ -19,5 +20,5 @@ module.exports = {
         client.clearInterval(interval);
         console.log('Removed interval.');
         console.log(client.activeIntervals);
-    }
-}
+    },
+};
