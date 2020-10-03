dc81d94228c00ac0331f8cb745abeb4f856fb354
Shams Jamal <syj67507@uga.edu>
2020-10-03 17:18:36 -0400

    logging in ping.js, kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -1,5 +1,6 @@
 const ExecutionError = require('../custom_errors/execution_error');
 const UsageError = require('../custom_errors/usage_error');
+const { logger, format } = require('../logger');
 
 module.exports = {
     name: 'kill',
@@ -10,6 +11,7 @@ module.exports = {
         const client = message.client;
 
         // validates user to be killed
+        logger.info(format('kill', 'kill.js - Validating arguments'));
         if (args.length != 2) {
             message.channel.send([
                 'UsageError: kill <@> <seconds>',
@@ -18,6 +20,7 @@ module.exports = {
             throw new UsageError('<seconds> argument not defined');
         }
         if (args[1] < 60) {
+            logger.warn(format('kill', 'kill.js - Amount of time is too small, setting to default'));
             args[1] = 60;
         }
         if (message.mentions.members.size == 0) {
@@ -28,13 +31,18 @@ module.exports = {
             message.channel.send(`I'm already killing ${args[0]}, give me time to prepare...`);
             throw new UsageError('User already specified. User must be revived.');
         }
+        logger.info(format('kill', 'kill.js - Argument validated.'));
 
         // finds user to kill and sets up reiterating interval
+        logger.info(format('kill', 'kill.js - Initiating...'));
         await message.channel.send(`Mr. Zurkon is here to kill: ${args[0]} :dagger:`);
         const guildMember = await message.mentions.members.first().fetch();
         if (`<@!${guildMember.user.id}>` != args[0]) {
             throw new ExecutionError('Fetching guild member does not match user mention');
         }
+        logger.info(format('kill', 'kill.js - Verified mentioned user'));
+        logger.debug(format('kill.js', `Mentioned User Id: ${guildMember.user.id}`));
+        logger.info(format('kill', 'kill.js - Setting interval'));
         const interval = client.setInterval(
             client.intervals.get('Kill Interval').execute,
             1000 * args[1], // time
@@ -42,9 +50,8 @@ module.exports = {
             args[0], // mention used for error handling
             client,
         );
-        console.log(args);
         client.activeIntervals.set(args[0], interval);
-        console.log(client.activeIntervals);
+        logger.debug(format('kill.js', `Added to intervals: ${client.activeIntervals}`));
 
     },
 };
--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,11 @@
+const { logger, format } = require("../logger");
+
 module.exports = {
     name: 'ping',
     description: 'Ping!',
     async execute(message, args) {
+        logger.debug(format('ping', 'Sending...'));
         message.channel.send('Pong.');
+        logger.debug(format('ping', 'Sent'));
     },
 };
