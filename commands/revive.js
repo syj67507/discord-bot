f6b14f962743c85f225a12e204f36329caf5e513
Shams Jamal <syj67507@uga.edu>
2020-10-16 20:28:42 -0400

    condensed code, no redundancy

--- commands/revive.js
+++ commands/revive.js
@@ -20,33 +20,37 @@ function processInput(message, args) {
         throw new UsageError('Did not mention anybody');
     }
 
-    // Returned mentioned users
-    if (message.mentions.members.size > 0) {
-        return message.mentions.members;
-    } else {
-        return null;
+    // Return 'all' of the ids
+    if (args.length === 1 && args[0] === 'all') {
+        return message.client.activeIntervals.keyArray();
     }
+
+    // Check for mentioned users
+    if (message.mentions.members.size < 1) {
+        throw new UsageError('Did not mention anybody');
+    }
+
+    return message.mentions.members.keyArray();
 }
 
-function destroyIntervals(message, mentions, args) {
-    // Clear explicit mentions
-    if (mentions !== null) {
-        for (const mention of mentions.keys()) {
-            const guildMemberId = mentions.get(mention).user.id;
-            const interval = message.client.activeIntervals.get(guildMemberId);
+/**
+ * Destroys the intervals that are set from the kill command for the given
+ * members.
+ *
+ * @param {Discord.Message} message Message that invoked this command.
+ * @param {Array}           members Array of the members to revive.
+ */
+function destroyIntervals(message, members) {
+    for (const memberId of members) {
+        // Clear the intervals for each provided member
+        if (message.client.activeIntervals.has(memberId)) {
+            const interval = message.client.activeIntervals.get(memberId);
             message.client.clearInterval(interval);
-            message.client.activeIntervals.delete(guildMemberId);
-            console.log(message.client.activeIntervals);
+            message.client.activeIntervals.delete(memberId);
         }
-    }
-
-    // Clear everyone if args is null
-    if (args.length == 1 && args[0] === 'all') {
-        for (const guildMemberId of message.client.activeIntervals.keys()) {
-            const interval = message.client.activeIntervals.get(guildMemberId);
-            message.client.clearInterval(interval);
-            message.client.activeIntervals.delete(guildMemberId);
-            console.log(message.client.activeIntervals);
+        // Notify if the member is not being killed
+        else {
+            message.channel.send(`<@${memberId}> is not on the hit list.`);
         }
     }
 }
@@ -59,35 +63,7 @@ module.exports = {
         `,
     async execute(message, args) {
         message.channel.send('revive');
-        const mentions = processInput(message, args);
-        destroyIntervals(message, mentions, args);
-        // Validating arguments
-        // logger.debug(format('revive', 'revive.js - Validating arguments'));
-        // if (args.length != 1) {
-        //     throw new UsageError('User mention not specified');
-        // }
-        // const client = message.client;
-        // if (!client.activeIntervals.has(args[0])) {
-        //     message.channel.send(
-        //         "Can't revive a member who isn't being killed."
-        //     );
-        //     throw new ExecutionError('User not being killed.');
-        // }
-        // logger.debug(format('revive', 'revive.js - Arguments validated'));
-
-        // logger.debug(
-        //     format('revive', 'revive.js - Reviving, removing from intervals...')
-        // );
-        // const interval = client.activeIntervals.get(args[0]);
-        // logger.debug(format('revive', `revive.js - interval: ${interval}`));
-        // client.activeIntervals.delete(args[0]);
-        // client.clearInterval(interval);
-        // logger.debug(format('revive', 'revive.js - Removed interval.'));
-        // logger.debug(
-        //     format(
-        //         'revive',
-        //         `revive.js - client.activeIntervals: ${client.activeIntervals}`
-        //     )
-        // );
+        const members = processInput(message, args);
+        destroyIntervals(message, members);
     },
 };
