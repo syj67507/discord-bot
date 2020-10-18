7e5a637112b0f18a790e25d761e2d33c620ebf5c
Shams Jamal <syj67507@uga.edu>
2020-10-18 16:11:29 -0400

    added tests

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -44,6 +44,11 @@ module.exports = {
             message.channel.send(msg);
         }
     },
+    test: {
+        processGuess,
+        verifyAnswer,
+        fetchRandomPokemon,
+    },
 };
 
 /**
