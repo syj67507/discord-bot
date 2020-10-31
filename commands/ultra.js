430168ea3378b058647ff6a0033481eeb9101ec5
Shams Jamal <syj67507@uga.edu>
2020-10-31 13:53:49 -0400

    fixed issues in ultra file for testing

--- commands/ultra.js
+++ commands/ultra.js
@@ -1,35 +1,30 @@
-const UsageError = require('../custom_errors/usage_error.js');
-const { logger, format } = require('../logger.js');
+const UsageError = require("../custom_errors/usage_error.js");
+const { logger, format } = require("../logger.js");
 
 module.exports = {
-    name: 'ultra',
-    description: 'Plays the clip...',
-    usage:
-        `
+    name: "ultra",
+    description: "Plays the clip...",
+    usage: `
         Must be in a voice channel to use the command
         ${process.env.PREFIX}ultra
         `,
     async execute(message, args) {
-<<<<<<< Updated upstream
-
-        const ytdl = require('ytdl-core');
-
-=======
->>>>>>> Stashed changes
         // Validation checks
-        logger.debug(format('ultra', 'Validating...'));
+        logger.debug(format("ultra", "Validating..."));
         if (message.member.voice.channel == null) {
-            message.channel.send('You must join a voice channel.');
-            throw new UsageError('Client cannot join a null value voice channel');
+            message.channel.send("You must join a voice channel.");
+            throw new UsageError(
+                "Client cannot join a null value voice channel"
+            );
         }
-        logger.debug(format('ultra', 'Validated'));
+        logger.debug(format("ultra", "Validated"));
 
         // Let the bot join the channel
-        logger.debug(format('ultra', 'Joining channel'));
+        logger.debug(format("ultra", "Joining channel"));
         const channel = message.member.voice.channel;
         const connection = await channel.join();
-        logger.debug(format('ultra', 'Joined channel'));
-        logger.debug(format('ultra', `Joined channel: ${channel.name}`));
+        logger.debug(format("ultra", "Joined channel"));
+        logger.debug(format("ultra", `Joined channel: ${channel.name}`));
 
         // Start yelling on successful join
         const dispatcher = connection.play(
@@ -37,18 +32,18 @@ module.exports = {
         );
 
         dispatcher.on("start", () => {
-            log.debug(f("ultra", "Playing clip"));
+            logger.debug(format("ultra", "Playing clip"));
         });
 
         dispatcher.on("error", (error) => {
-            log.error(f("ultra", "Error in playing the clip."));
-            log.error(f("ultra", error));
+            logger.error(format("ultra", "Error in playing the clip."));
+            logger.error(format("ultra", error));
         });
 
         dispatcher.on("finish", () => {
-            log.debug(f("ultra", "Leaving channel..."));
+            logger.debug(format("ultra", "Leaving channel..."));
             channel.leave();
-            log.debug(f("ultra", "Left the channel"));
+            logger.debug(format("ultra", "Left the channel"));
         });
     },
 };
