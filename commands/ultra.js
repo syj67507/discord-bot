a89d9bdd461950bd25c0534c946d42302229473b
Shams Jamal <syj67507@uga.edu>
2020-10-31 14:25:56 -0400

    fixed logging name issues from merge in ultra command

--- commands/ultra.js
+++ commands/ultra.js
@@ -11,21 +11,21 @@ module.exports = {
         `,
     async execute(message, args) {
         // Validation checks
-        logger.debug(format("ultra", "Validating..."));
+        log.debug(f("ultra", "Validating..."));
         if (message.member.voice.channel == null) {
             message.channel.send("You must join a voice channel.");
             throw new UsageError(
                 "Client cannot join a null value voice channel"
             );
         }
-        logger.debug(format("ultra", "Validated"));
+        log.debug(f("ultra", "Validated"));
 
         // Let the bot join the channel
-        logger.debug(format("ultra", "Joining channel"));
+        log.debug(f("ultra", "Joining channel"));
         const channel = message.member.voice.channel;
         const connection = await channel.join();
-        logger.debug(format("ultra", "Joined channel"));
-        logger.debug(format("ultra", `Joined channel: ${channel.name}`));
+        log.debug(f("ultra", "Joined channel"));
+        log.debug(f("ultra", `Joined channel: ${channel.name}`));
 
         // Start yelling on successful join
         const dispatcher = connection.play(
@@ -33,18 +33,18 @@ module.exports = {
         );
 
         dispatcher.on("start", () => {
-            logger.debug(format("ultra", "Playing clip"));
+            log.debug(f("ultra", "Playing clip"));
         });
 
         dispatcher.on("error", (error) => {
-            logger.error(format("ultra", "Error in playing the clip."));
-            logger.error(format("ultra", error));
+            log.error(f("ultra", "Error in playing the clip."));
+            log.error(f("ultra", error));
         });
 
         dispatcher.on("finish", () => {
-            logger.debug(format("ultra", "Leaving channel..."));
+            log.debug(f("ultra", "Leaving channel..."));
             channel.leave();
-            logger.debug(format("ultra", "Left the channel"));
+            log.debug(f("ultra", "Left the channel"));
         });
     },
 };
