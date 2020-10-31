213c64fb2a5f2ad70d90efadb45e3dbb5291aac6
Shams Jamal <syj67507@uga.edu>
2020-10-31 14:47:15 -0400

    ran prettier --config ./.prettierrc -w .

--- custom/ExecutionError.js
+++ custom/ExecutionError.js
@@ -1,13 +1,11 @@
 class ExecutionError extends Error {
-
     constructor(...params) {
         super(...params);
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, ExecutionError);
         }
-        this.name = 'ExecutionError';
+        this.name = "ExecutionError";
     }
-
 }
 
 module.exports = ExecutionError;
