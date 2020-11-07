7fee9fc6d92207dc0925b6a70887af8afca966c1
Shams Jamal <syj67507@uga.edu>
2020-11-06 22:34:44 -0500

    refactored joke to use axios

--- commands/joke.js
+++ commands/joke.js
@@ -1,3 +1,5 @@
+const { executionAsyncResource } = require("async_hooks");
+const axios = require("axios");
 const log = require("../custom/logger.js").logger;
 const f = require("../custom/logger.js").format;
 
@@ -8,43 +10,19 @@ module.exports = {
         ${process.env.PREFIX}joke
         `,
     async execute(message, args) {
-        log.debug(f("joke", "Making a request..."));
-        const https = require("https");
-        https
-            .get("https://icanhazdadjoke.com/slack", (res) => {
-                // Receiving joke
-                log.debug(f("joke", "Loading joke..."));
-                let data = "";
-                res.on("data", (chunk) => {
-                    data += chunk;
-                });
-                log.debug(f("joke", `data: ${data}`));
-
-                // Found joke! Send in channel
-                log.debug(f("joke", "Found the joke"));
-                res.on("end", () => {
-                    let joke = JSON.parse(data);
-                    log.debug(f("joke", `joke: ${joke}`));
-                    if (joke != null) {
-                        joke = joke.attachments[0].text;
-                        log.debug(f("joke", "Processing joke"));
-                        log.debug(f("joke", `joke: ${joke}`));
-                        if (Math.floor(Math.random() * 2) == 0) {
-                            joke += " :rofl:";
-                        } else {
-                            joke += " :joy:";
-                        }
-                    } else {
-                        joke = "Sorry... we couldn't get you a joke :(";
-                    }
-                    log.debug(f("joke", "Sending joke..."));
-                    log.debug(f("joke", `joke: ${joke}`));
-                    message.channel.send(joke);
-                });
-            })
-            .on("error", (error) => {
-                message.channel.send("Sorry... we couldn't get you a joke :(");
-                throw error;
-            });
+        try {
+            const res = await axios.get("https://icanhazdadjoke.com/slack");
+            let joke = res.data.attachments[0].text;
+            if (Math.floor(Math.random() * 2) == 0) {
+                joke += " :rofl:";
+            } else {
+                joke += " :joy:";
+            }
+            message.channel.send(joke);
+        } catch {
+            message.channel.send(
+                "Sorry... there I couldn't get you a joke :frowning:"
+            );
+        }
     },
 };
