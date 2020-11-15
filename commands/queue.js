b93659dc17d8c14312b9930f027f319cc2001b7f
Shams Jamal <syj67507@uga.edu>
2020-11-15 10:44:25 -0500

    added logs

--- commands/queue.js
+++ commands/queue.js
@@ -8,23 +8,38 @@ module.exports = {
         ${process.env.PREFIX}queue <YouTube Link>
         `,
     async execute(message, args) {
+        log.debug(f("queue", "Checking for flags..."));
+
         // Checks if the user wants to clear the queue
         if (args.includes("clear") || args.includes("-c")) {
+            log.debug(f("queue", "Found the clear flag."));
             message.client.musicQueue = [];
             message.reply(["The queue has been cleared!"]);
+            log.debug(f("queue", "Cleared the queue."));
+            log.debug(
+                f(
+                    "queue",
+                    "Songs in queue: " + message.client.musicQueue.length
+                )
+            );
             return;
         }
 
         // Checks if the user wants to display the queue
         if (args.includes("display") || args.includes("-d")) {
+            log.debug(f("queue", "Found the display flag."));
             message.reply([
                 "Songs in queue: " + message.client.musicQueue.length,
             ]);
+            log.debug(f("queue", "Displayed queue length."));
             return;
         }
 
+        log.debug(f("queue", "Validating song link..."));
         const songLink = validateLink(message, args);
+        log.debug(f("queue", "Adding to queue..."));
         addToQueue(message.client.musicQueue, songLink);
+        log.debug(f("queue", "Queue successful."));
 
         message.reply([
             "Queued up!",
