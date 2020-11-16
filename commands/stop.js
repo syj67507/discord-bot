cb16bf0b8f1c8722628129afbb1e7e2315ffa3d8
Shams Jamal <syj67507@uga.edu>
2020-11-15 22:42:27 -0500

    created stop command, validation, and logs

---
+++ commands/stop.js
@@ -0,0 +1,35 @@
+const { logger: log, format: f } = require("../custom/logger");
+const UsageError = require("../custom/UsageError");
+const ExecutionError = require("../custom/ExecutionError");
+
+module.exports = {
+    name: "stop",
+    description: "The bot stops playing music and clears the queue.",
+    usage: `
+        ${process.env.PREFIX}stop
+        `,
+    async execute(message, args) {
+        // Check if the bot is in a voice channel
+        log.debug(f("stop", "Checking for client's voice connection..."));
+        if (message.client.voice.connections.size === 0) {
+            log.debug(
+                f(
+                    "stop",
+                    "Client is not playing anything, throwing UsageError..."
+                )
+            );
+            message.channel.send("I am not playing anything!");
+            throw new UsageError("Cannot stop when client isn't playing.");
+        }
+        log.debug(f("stop", "Found voice connection!"));
+        // Leaves the channel, throws error on failure.
+        try {
+            log.debug(f("stop", "Attempting to leave..."));
+            message.client.voice.connections.first().channel.leave();
+            log.debug(f("stop", "Left the channel"));
+        } catch (error) {
+            log.error(f("stop", `${error.name}: ${error.message}`));
+            throw new ExecutionError("Leaving the voice channel failed.");
+        }
+    },
+};
