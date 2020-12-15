804898487c3ed26c0c19f18edf576706cf85fb56
Shams Jamal <syj67507@uga.edu>
2020-12-15 18:28:14 -0500

    removed tests using mock client

--- tests/queue.test.js
+++ tests/queue.test.js
@@ -1,16 +1,6 @@
 const { addToQueue } = require("../commands/queue");
-const { MockDiscord, MockMessage } = require("./mocks/MockDiscord.js");
-
-// setting up the Discord mock
-const mock = new MockDiscord();
-const user = { id: 9, username: "username", discriminator: "1234" };
-
-afterAll(() => {
-    mock.client.destroy();
-});
 
 test("Testing addToQueue function", () => {
-    const message = new MockMessage("", mock.channel, {});
     let musicQueue = [];
     expect(addToQueue(musicQueue, "song link")).toBeUndefined();
     expect(() => {
