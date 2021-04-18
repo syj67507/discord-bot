381f135beda9eeba3f5181eb6d44125d965d0d56
Shams Jamal <shams.jamal67@gmail.com>
2021-04-18 19:13:12 -0400

    added interval to cycle nicknames

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,43 @@ client.once("ready", () => {
 client.login(token);
 
 MusicManager.getInstance(client); // Initializing the music manager
+
+// Cycling through legal names on No Hands Server
+let lastDateChanged: Date = new Date();
+client.setInterval(async () => {
+    // Check if it is time to change the nickname
+    const d: Date = new Date();
+    console.log(d.toUTCString());
+    if (lastDateChanged.getUTCDate() === d.getUTCDate()) {
+        return;
+    }
+    lastDateChanged = d;
+
+    const names: string[] = [
+        "Imran Rahman",
+        "Brandon Nyguen",
+        "Jacob Yu",
+        "Nicholas Colin Kemme",
+        "Fahad Islam",
+        "Fahim Zaman",
+        "Shams Jamal",
+        "Ishmam Mahmud",
+    ];
+
+    // Fetch the client as a guildMember
+    const noHandsGuildId = "749330283081236532";
+    const clientId = "763939181570949142";
+    const guild = await client.guilds.fetch(noHandsGuildId);
+    const guildMember = await guild.members.fetch(clientId);
+
+    // Find index of current nickname and set
+    let index = names.findIndex((name) => {
+        return name === guildMember.nickname;
+    });
+    if (index === -1) {
+        index = Math.floor(Math.random() * names.length);
+    } else {
+        index = index === 7 ? 0 : (index += 1); // Rolls back to the beginning
+    }
+    await guildMember.setNickname(names[index]);
+}, 3600000);
