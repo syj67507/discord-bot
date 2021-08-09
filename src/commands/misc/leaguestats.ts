a27b07d58f89eba1d57d22521c5cf0c64266cad4
encrxption <imranrbusiness@gmail.com>
2021-08-09 12:10:40 -0400

    Requested changes

--- src/commands/misc/leaguestats.ts
+++ src/commands/misc/leaguestats.ts
@@ -7,31 +7,34 @@ import {
 import { link } from "fs";
 
 module.exports = class LeagueStatsCommand extends (Command) {
-    // Configuration
     constructor(client: CommandoClient) {
         super(client, {
             name: "leaguestats",
             group: "misc",
             memberName: "leaguestats",
             description: "Sends the op.gg profile of requested user (North American Region)",
-            aliases: ["ls"],
+            aliases: ["ls", "opgg"],
             args: [{
                 type: "string",
-                key: "userInput",
+                key: "leagueUsernameInput",
                 default: "",
-                prompt: "Username of requsted league account.",
+                prompt: "Username of requested league account.",
             },]
         });
     }
 
-    // What makes the command do stuff
     async run(message: CommandoMessage, args: any) {
-        const leagueURL: URL = searchLeagueName(args.userInput);
+        const leagueURL: URL = searchLeagueName(args.leagueUsernameInput);
         message.say(leagueURL.href);
         return null;
     }
 };
 
+/**
+ * Attaches username to the end of the op.gg URL to redirect to exact summoner page.
+ * @param {string} input The username inputted by the user
+ * @returns {URL} The new URL with the summoner name attached to the end
+ */
 function searchLeagueName(input: string) {
     const leagueURL = new URL('https://na.op.gg/summoner/userName=' + input);
     return leagueURL;
