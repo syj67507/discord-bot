b27ff383a71a9f54c9df8cbdef53c3176219e411
Shams Jamal <shams.jamal67@gmail.com>
2021-08-30 19:52:38 -0400

    migrated joke command and wrote tests

---
+++ v12_src/commands/misc/joke.ts
@@ -0,0 +1,41 @@
+import { Message } from "discord.js";
+import { Command } from "../../custom/base";
+import axios from "axios";
+import { logger as log, format as f } from "../../custom/logger";
+
+const jokeCommand: Command = {
+    name: "joke",
+    aliases: ["dadjoke", "jokes"],
+    description: "Sends a dad joke!",
+    arguments: [],
+    enabled: true,
+    async run(message: Message) {
+        log.debug(f("joke", "Fetching the joke..."));
+        const res = await axios.get("https://icanhazdadjoke.com/slack");
+        let joke: string = res.data.attachments[0].text;
+
+        log.debug(f("joke", "Processing response..."));
+        joke = appendEmoji(joke);
+        log.debug(f("joke", `Joke: ${joke}`));
+
+        log.debug(f("joke", "Sending to channel..."));
+        message.reply(joke);
+        return null;
+    },
+};
+
+/**
+ * Appends an emoji to the end of the joke.
+ * @param {string} joke The joke returned from the axios call
+ * @returns {string} The joke with the emoji appended at the end
+ */
+export function appendEmoji(joke: string): string {
+    if (Math.floor(Math.random() * 2) == 0) {
+        joke += " :rofl:";
+    } else {
+        joke += " :joy:";
+    }
+    return joke;
+}
+
+export default jokeCommand;
