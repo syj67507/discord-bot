23333632665ab05861b42f26d2eb0755b9559451
Shams Jamal <shams.jamal67@gmail.com>
2021-10-30 21:57:33 -0400

    migrated queue command

---
+++ v12_src/commands/music/queue.ts
@@ -0,0 +1,100 @@
+import { Message } from "discord.js";
+import { ArgumentValues, Command } from "../../custom/base";
+import { format as f, logger as log } from "../../custom/logger";
+import MusicManager, { Track } from "../../custom/music-manager";
+
+const queueCommand: Command = {
+    name: "queue",
+    description: "Queues a music track.",
+    aliases: ["q"],
+    arguments: [
+        {
+            key: "track",
+            type: "strings",
+            description:
+                "The search phrase for YouTube search or the direct YouTube link",
+            default: "",
+        },
+    ],
+    enabled: true,
+    async run(message: Message, args: ArgumentValues): Promise<null> {
+        const mm = MusicManager.getInstance(message.client);
+
+        // Displays queue on no track name passed
+        const playlist = ["Currently Queued: "];
+        if (args.track === "") {
+            log.debug(f("queue", "Displaying queue..."));
+            if (mm.queueLength() === 0) {
+                message.reply("No tracks left in queue.");
+                return null;
+            }
+
+            // Fetching all tracks in queue/playlist
+            for (const track of mm.playlist) {
+                playlist.push(track.title);
+            }
+            message.reply(playlist);
+            return null;
+        }
+
+        // Clears queue if specified
+        if (args.track === "clear") {
+            log.debug(f("queue", "Clearing queue..."));
+            mm.clearQueue();
+            log.debug(f("queue", "Queue cleared."));
+            message.reply("Queue has been cleared.");
+            return null;
+        }
+
+        log.debug(f("queue", "Checking if argument is a YouTube link"));
+        let track: Track;
+        if (mm.isYTLink(args.track as string)) {
+            log.debug(f("queue", "Argument is a YouTube link"));
+            try {
+                track = await mm.createTrackFromYTLink(args.track as string);
+                mm.queue(track);
+            } catch (error) {
+                if (error instanceof Error) {
+                    log.error(f("queue", error.message));
+                } else {
+                    log.error(f("queue", "Unknown error."));
+                }
+                log.error(
+                    f("queue", "Unable to create Track object" + " from youtube link.")
+                );
+                message.reply(
+                    "Unable to queue with the provided link. " +
+                        "Only YouTube links are supported. " +
+                        "If the link is a youtube link, make sure it is valid."
+                );
+                return null;
+            }
+            message.reply(`Queued: ${track.title}`);
+            return null;
+        } else {
+            log.debug(f("queue", "Argument is NOT a link"));
+            log.debug(f("queue", `Searching YT for ${args.track}`));
+            try {
+                track = await mm.search(args.track as string);
+                mm.queue(track);
+                log.debug(f("queue", `Queued: ${track.link}`));
+                message.reply(`Queued: ${track.title}`);
+                return null;
+            } catch (error) {
+                if (error instanceof Error) {
+                    log.error(f("queue", error.message));
+                } else {
+                    log.error(f("queue", "Unknown error."));
+                }
+                message.reply([
+                    `Couldn't find a link for \`${args.track}\``,
+                    "Either the search had no results or the search failed.",
+                    "A restart may be necessary...@Bonk",
+                ]);
+                return null;
+            }
+        }
+    },
+};
+
+export default queueCommand;
