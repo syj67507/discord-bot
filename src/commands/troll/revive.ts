a15027cb13de06a4e17732b5b778a2bbc953e126
Shams Jamal <shams.jamal67@gmail.com>
2021-12-18 13:22:54 -0500

    converted revive

--- src/commands/troll/revive.ts
+++ src/commands/troll/revive.ts
@@ -1,52 +1,49 @@
-// import { KillIntervals } from "../../custom/storage";
-// import { GuildMember, Message } from "discord.js";
-// import { ArgumentValues, Command } from "../../custom/base";
-// import { format as f, logger as log } from "../../custom/logger";
+import { KillIntervals } from "../../custom/storage";
+import { CommandInteraction, GuildMember } from "discord.js";
+import { Command, OptionTypes } from "../../custom/base";
+import { format as f, logger as log } from "../../custom/logger";
+import { clearInterval } from "timers";
 
-// const reviveCommand: Command = {
-//     name: "revive",
-//     description: "Revives a member from that is being killed.",
-//     enabled: true,
-//     arguments: [
-//         {
-//             key: "members",
-//             description: "Who is to be revived?",
-//             type: "user",
-//         },
-//     ],
-//     async run(message: Message, args: ArgumentValues): Promise<null> {
-//         const intervals = KillIntervals.getInstance();
-//         const owners: string[] = ["108726505688862720"];
-//         // for (const user of message.client.owners) {
-//         //     owners.push(user.id);
-//         // }
+const reviveCommand: Command = {
+    name: "revive",
+    description: "Revives a member from that is being killed.",
+    enabled: true,
+    options: [
+        {
+            name: "user",
+            description: "Who is to be revived?",
+            type: OptionTypes.USER,
+            required: false,
+        },
+    ],
+    async run(interaction: CommandInteraction, options: any): Promise<null> {
+        const intervals = KillIntervals.getInstance();
+        const owners: string[] = ["108726505688862720"]; // @Bonk
 
-//         for (const member of [args.members as GuildMember]) {
-//             log.debug(f("revive", `Member ID: ${member}`));
-//             if (owners.includes(message.member!.id)) {
-//                 log.debug(f("revive", "Client owner override."));
-//             }
-//             if (
-//                 member.id === message.member!.id &&
-//                 !owners.includes(message.member!.id)
-//             ) {
-//                 message.reply("You cannot revive yourself");
-//                 return null;
-//             }
-//             if (!intervals.has(member.id)) {
-//                 message.reply(`${member} not being killed.`);
-//                 return null;
-//             }
+        log.debug(f("revive", `Member Id to revive: ${options.user}`));
+        const invoker = interaction.member as GuildMember;
+        if (owners.includes(invoker.id)) {
+            log.debug(f("revive", "Client owner override."));
+        }
+        if (options.user.id === invoker.id && !owners.includes(invoker.id)) {
+            interaction.reply("You cannot revive yourself");
+            return null;
+        }
+        if (!intervals.has(options.user.id)) {
+            interaction.reply(`${options.user} not being killed.`);
+            return null;
+        }
 
-//             // Member found, revive...
-//             const interval = intervals.get(member.id);
-//             message.client.clearInterval(interval!);
-//             log.debug(f("revive", `Interval cleared: ${member.id}`));
-//             message.channel.send(`${member} revived.`);
-//             intervals.delete(member.id);
-//         }
-//         return null;
-//     },
-// };
+        // Member found, revive...
+        const interval = intervals.get(options.user.id);
 
-// export default reviveCommand;
+        clearInterval(interval!);
+        log.debug(f("revive", `Interval cleared: ${options.user.id}`));
+        interaction.reply(`${options.user} revived.`);
+        intervals.delete(options.user.id);
+
+        return null;
+    },
+};
+
+export default reviveCommand;
