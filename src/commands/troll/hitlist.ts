4bdd4d20f58c34de7d7d9bf123b8e90acab82232
encrxption <imranrbusiness@gmail.com>
2021-12-25 13:02:12 -0500

    Finalized hitlist command

--- src/commands/troll/hitlist.ts
+++ src/commands/troll/hitlist.ts
@@ -3,32 +3,28 @@ import { Message } from "discord.js";
 import { ArgumentValues, Command } from "../../custom/base";
 
 const HitListCommand: Command = {
-            name: "hitlist",
-            description: "Displays killed people in the server in a hitlist.",
-            aliases: ["hl", "killList"],
-            arguments: [],
-        enabled: true,
-        async run(message: Message, args: any) {
-             message.channel.send("People currently killed in the server: " + killedPeople());
-             return null;
-    }
+    name: "hitlist",
+    description: "Displays killed people in the server in a hitlist.",
+    aliases: ["hl", "killList"],
+    arguments: [],
+    enabled: true,
+
+    async run(message: Message, args: any) {
+        const intervals = KillIntervals.getInstance();
+        const keys = intervals.keys();
+        const members = [];
+        console.log(intervals.size);
+
+        if (intervals.size == 0) {
+            message.channel.send("No one is currently killed in the server!");
+        } else {
+            for (const id of keys) {
+                members.push("<@!" + id + ">");
+                message.channel.send("People currently killed in the server: " + members);
+            }
+        }
+        return null;
+    },
 };
 
-
-/**
- * Returns values of killed users of a server in an array.
- * @returns {string[]} array of killed users in a server.
- */
-
-export function killedPeople() {
-    const intervals = KillIntervals.getInstance();
-    const keys = intervals.keys();
-    const members = [];
-
-    for(const id of keys) {
-        members.push("<@!" + id + ">");
-    }
-
-    return members;
-
-}
+export default HitListCommand;
