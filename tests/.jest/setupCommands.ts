2ce2fa49202504f92ee942a45c4514220a753492
Shams Jamal <shams.jamal67@gmail.com>
2022-01-12 16:18:56 -0500

    updated setupCommands to load all commands from application

--- tests/.jest/setupCommands.ts
+++ tests/.jest/setupCommands.ts
@@ -4,8 +4,32 @@
 
 import { Collection } from "discord.js";
 import { Command } from "../../src/custom/base";
+import fs from "fs";
+import path from "path";
 
 const commands = new Collection<string, Command>();
+const commandGroups = new Collection<string, string[]>();
+
+// loading all commands defined in application
+const groups = fs.readdirSync(path.resolve(`${__dirname}/../../src/commands`));
+for (const group of groups) {
+    const commandFiles = fs.readdirSync(
+        path.resolve(`${__dirname}/../../src/commands/${group}`)
+    );
+    commandGroups.set(group, []);
+    for (const commandFile of commandFiles) {
+        const p = `${__dirname}/../../src/commands/${group}/${commandFile}`.replace(
+            ".ts",
+            ""
+        );
+
+        const command: Command = require(p).default;
+        if (command) {
+            commands.set(command.name, command);
+            commandGroups.get(group)!.push(command.name);
+        }
+    }
+}
 
 commands.set("sample", {
     name: "sample",
@@ -29,9 +53,8 @@ commands.set("sampleUtility", {
     options: [],
 });
 
-const commandGroups = new Collection<string, string[]>();
-commandGroups.set("misc", ["sample"]);
-commandGroups.set("utility", ["sampleUtility"]);
+commandGroups.get("misc").push("sample");
+commandGroups.get("utility").push("sampleUtility");
 
 global["commands"] = commands;
 global["commandGroups"] = commandGroups;
