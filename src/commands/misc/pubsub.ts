75c2e3271e9199342c04c93fe7e8383efd8cd44a
imran <imranrbusiness@gmail.com>
2022-02-12 16:02:41 -0500

    pubsub.ts

--- src/commands/misc/pubsub.ts
+++ src/commands/misc/pubsub.ts
@@ -1,22 +1,24 @@
 import { Message } from "discord.js";
 import { Command } from "../../custom/base";
 import axios from "axios";
-import { logger as log, format as f } from "../../custom/logger";
+
+const html2json = require('html2json').html2json;
 
 const pubsubCommand: Command = {
     name: "pubsub",
-    description: "Responds with if the Publix chicken tender sub is on sale or not.",
+    description: "Responds with what publix sub is on sale for the week and how much it is.",
     aliases: ["publixsublix", "publix"],
     enabled: true,
     arguments: [],
     async run(message: Message) {
-        const response = await axios.get("https://arepublixchickentendersubsonsale.com/");
-        log.debug(f("pubsub", response.data));
-        if (response.data.includes("Not this week my dudes")) {
-            message.channel.send("The Publix chicken tender sandwich is NOT on sale.");
-        } else {
-            message.channel.send("The publix chicken tender sandwich is on sale.");
-        }
+        const response = await axios.get("https://pubsub.club/")
+        const htmlResponse = response.data.replace('<!doctype html>', '');
+        const jsonResponse = (html2json(htmlResponse));
+        const name = JSON.stringify((jsonResponse.child[0].child[1].child[0].child[1].child[0].child[0].text),null,2);
+        const newName = name.replace("&#39;","'").replace(/"/g,'');
+        const price = JSON.stringify((jsonResponse.child[0].child[1].child[0].child[1].child[1].child[0].text),null,2);
+        const newPrice = price.replace(/"/g,'');
+        message.channel.send("Name of sub on sale this week: " + newName + "\n" + "Price: " + newPrice);
         return null;
     },
 };
