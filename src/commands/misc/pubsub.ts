7c384888eb1406479d3054f07a0d181b048b2b3c
Shams Jamal <shams.jamal67@gmail.com>
2022-02-13 11:31:51 -0500

    clean up

--- src/commands/misc/pubsub.ts
+++ src/commands/misc/pubsub.ts
@@ -2,23 +2,32 @@ import { Message } from "discord.js";
 import { Command } from "../../custom/base";
 import axios from "axios";
 
-const html2json = require('html2json').html2json;
+// Does not have TypeScript support, using require syntax
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { html2json } = require("html2json");
 
 const pubsubCommand: Command = {
     name: "pubsub",
-    description: "Responds with what publix sub is on sale for the week and how much it is.",
+    description:
+        "Responds with what publix sub is on sale for the week and how much it is.",
     aliases: ["publixsublix", "publix"],
     enabled: true,
     arguments: [],
     async run(message: Message) {
-        const response = await axios.get("https://pubsub.club/")
-        const htmlResponse = response.data.replace('<!doctype html>', '');
-        const jsonResponse = (html2json(htmlResponse));
-        const name = JSON.stringify((jsonResponse.child[0].child[1].child[0].child[1].child[0].child[0].text),null,2);
-        const newName = name.replace("&#39;","'").replace(/"/g,'');
-        const price = JSON.stringify((jsonResponse.child[0].child[1].child[0].child[1].child[1].child[0].text),null,2);
-        const newPrice = price.replace(/"/g,'');
-        message.channel.send("Name of sub on sale this week: " + newName + "\n" + "Price: " + newPrice);
+        const response = await axios.get("https://pubsub.club/");
+        const htmlResponse = response.data.replace("<!doctype html>", "");
+
+        const json = html2json(htmlResponse);
+        const name = json.child[0].child[1].child[0].child[1].child[0].child[0].text
+            .replace("&#39;", "'")
+            .replace(/"/g, "");
+        const price =
+            json.child[0].child[1].child[0].child[1].child[1].child[0].text.replace(
+                /"/g,
+                ""
+            );
+
+        message.channel.send(`**${name}**\nPrice: ${price}`);
         return null;
     },
 };
