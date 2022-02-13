5125624a6d4d7cfb4f27e4307057bc1e673ab6dc
Shams Jamal <shams.jamal67@gmail.com>
2022-02-13 14:34:29 -0500

    initial rework of connect and disconnect

--- src/commands/misc/meow.ts
+++ src/commands/misc/meow.ts
@@ -12,6 +12,7 @@ import {
 } from "@discordjs/voice";
 import path from "path";
 import { Command } from "../../custom/base";
+import MusicManager, { Track } from "../../custom/music-manager";
 
 const meowCommand: Command = {
     name: "meow",
@@ -20,66 +21,82 @@ const meowCommand: Command = {
     enabled: true,
     options: [],
     async run(interaction: CommandInteraction) {
-        if (interaction.member instanceof GuildMember === false) {
-            // Unable to do anything because data object do not line up
-            console.log("Unable to do anything because data object do not line up");
-            return null;
-        }
-
-        const member = interaction.member as GuildMember;
-        if (member.voice.channel === null) {
-            console.log("You are not connected to a voice channel");
-            return null;
-        }
-        joinVoiceChannel({
-            channelId: member.voice.channel.id,
-            guildId: member.voice.channel.guild.id,
-            // casted due to typing issues (https://github.com/discordjs/voice/issues/166)
-            adapterCreator: member.voice.channel.guild
-                .voiceAdapterCreator as DiscordGatewayAdapterCreator,
-        });
-        const connection = getVoiceConnection(member.voice.channel.guild.id)!;
-        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
-            console.log(oldState.status, newState.status);
-            try {
-                await Promise.race([
-                    entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-                    entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-                ]);
-                // Seems to be reconnecting to a new channel - ignore disconnect
-            } catch (error) {
-                // Seems to be a real disconnect which SHOULDN'T be recovered from
-                connection.destroy();
-            }
-        });
-        console.log(__dirname);
-        try {
-            await entersState(connection!, VoiceConnectionStatus.Ready, 5_000);
-            console.log("ready to play audio");
-
-            const player = createAudioPlayer();
-            player.on("stateChange", (oldState, newState) => {
-                console.log("Audio player:", oldState.status, "->", newState.status);
-            });
-            const resourcePath = path.join(
-                __dirname,
-                "..",
-                "..",
-                "..",
-                "media",
-                "prominence-burn-1c9da3a6.1280x720r.mp4"
-            );
-            console.log(resourcePath);
-            const resource = createAudioResource(resourcePath);
-            player.play(resource);
-
-            connection.subscribe(player);
-        } catch (error) {
-            console.error(error);
-        }
-        await interaction.reply("Meow!");
+        const mm = MusicManager.getInstance(interaction.client);
+        mm.connect((interaction.member as GuildMember)!.voice!.channel!);
+        const track: Track = {
+            duration: "",
+            title: "title",
+            link: "asdf",
+        };
+        console.log(mm.getPlaylist());
+        mm.queue(track);
+        console.log(mm.getPlaylist());
+        await new Promise((r) => setTimeout(r, 2000));
+        mm.disconnect(interaction.guildId!);
         return null;
     },
 };
+
+async function research(interaction: CommandInteraction) {
+    if (interaction.member instanceof GuildMember === false) {
+        // Unable to do anything because data object do not line up
+        console.log("Unable to do anything because data object do not line up");
+        return null;
+    }
+
+    const member = interaction.member as GuildMember;
+    if (member.voice.channel === null) {
+        console.log("You are not connected to a voice channel");
+        return null;
+    }
+    joinVoiceChannel({
+        channelId: member.voice.channel.id,
+        guildId: member.voice.channel.guild.id,
+        // casted due to typing issues (https://github.com/discordjs/voice/issues/166)
+        adapterCreator: member.voice.channel.guild
+            .voiceAdapterCreator as DiscordGatewayAdapterCreator,
+    });
+    const connection = getVoiceConnection(member.voice.channel.guild.id)!;
+    connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+        console.log(oldState.status, newState.status);
+        try {
+            await Promise.race([
+                entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+                entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+            ]);
+            // Seems to be reconnecting to a new channel - ignore disconnect
+        } catch (error) {
+            // Seems to be a real disconnect which SHOULDN'T be recovered from
+            connection.destroy();
+        }
+    });
+
+    try {
+        await entersState(connection!, VoiceConnectionStatus.Ready, 5_000);
+        console.log("ready to play audio");
+
+        const player = createAudioPlayer();
+        player.on("stateChange", (oldState, newState) => {
+            console.log("Audio player:", oldState.status, "->", newState.status);
+        });
+        const resourcePath = path.join(
+            __dirname,
+            "..",
+            "..",
+            "..",
+            "media",
+            "prominence-burn-1c9da3a6.1280x720r.mp4"
+        );
+        console.log(resourcePath);
+        const resource = createAudioResource(resourcePath);
+        player.play(resource);
+
+        connection.subscribe(player);
+    } catch (error) {
+        console.error(error);
+    }
+    await interaction.reply("Meow!");
+    return null;
+}
 
 export default meowCommand;
