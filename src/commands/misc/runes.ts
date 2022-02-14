a7adbc5f76939f511211156c32d04a1e68efff30
sadiyan <sadiy4n@gmail.com>
2022-02-13 22:25:37 -0500

    removed TODO/commented code, fixed concatenation, removed function championString() and replaced with arr variable

--- src/commands/misc/runes.ts
+++ src/commands/misc/runes.ts
@@ -1,8 +1,5 @@
 import { Command } from "../../custom/base";
 import { Message } from "discord.js";
-// If the following line is causing issues
-// run `npm install @fightmegg/riot-api`
-// then delete these comments
 import { DDragon } from "@fightmegg/riot-api";
 
 const runesCommand: Command = {
@@ -18,8 +15,6 @@ const runesCommand: Command = {
     ],
 
     async run(message: Message, args: any) {
-        // console.log("champion:", champion);
-        // console.log(await getAllChampions())
         const allChampions = await getAllChampions();
         const userInput = args.championName as string;
 
@@ -39,21 +34,6 @@ const runesCommand: Command = {
             message.reply("Please enter a valid champion!");
             return null;
         }
-        // TODO:
-        // 1. Get all the LoL champions
-        // 2. Verify that the provided champion name from user input is a valid LoL champion
-        //    If the champion name is not valid, then reply with an error message
-        // 3. Make a rune page link
-        // 4. Reply to the user with the rune page link
-
-        // HINTS:
-        // - Remember that the following run() function/method is called whenever you use this
-        //   command
-        // - The input from the text channel when using a command will be stored in the {champion}
-        //   variable in the first line of the run() function/method
-        // - Each function/method is intended to be small part of the whole command. Read the
-        //   documents above each "helper" function to find out what it is supposed to do and figure how
-        //   to use them together in the function
     },
 };
 
@@ -65,36 +45,21 @@ const runesCommand: Command = {
 async function getAllChampions(): Promise<string[]> {
     const ddragon = new DDragon();
     const rawChampionInfo = await ddragon.champion.all();
-
     const champions = Object.keys(rawChampionInfo.data);
 
-    function championString() {
-        const arr = [];
-        for (let i = 0; i < champions.length; i++) {
-            const champion = rawChampionInfo.data[champions[i]];
-            let name = champion.name;
-            if (name.includes(" ")) {
-                name = name.replace(" ", "");
-            } else if (name.includes("'")) {
-                name = name.replace("'", "");
-            }
-            name = name.toLowerCase();
-            arr.push(name);
+    const arr = [];
+    for (let i = 0; i < champions.length; i++) {
+        const champion = rawChampionInfo.data[champions[i]];
+        let name = champion.name;
+        if (name.includes(" ")) {
+            name = name.replace(" ", "");
+        } else if (name.includes("'")) {
+            name = name.replace("'", "");
         }
-        return arr;
+        name = name.toLowerCase();
+        arr.push(name);
     }
-
-    // TODO:
-    // Champs contains an object that looks like the following document
-    // https://ddragon.leagueoflegends.com/cdn/11.18.1/data/en_US/champion.json
-    // Here find a way to transform this object (rawChampionInfo)
-    // into an an array of strings where each index is a champion and store it into
-    // a (champions) variable.
-    // HINT: Use the following page on Objects for what you could possibly use
-    // Objects use key: value pairs to store data
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
-
-    return championString();
+    return arr;
 }
 
 /**
@@ -104,11 +69,8 @@ async function getAllChampions(): Promise<string[]> {
  * @returns {string} A link to the champion's most popular rune page
  */
 function buildRunePageLink(champion: string): string {
-    // TODO:
-    // Using the parameter {champion}, create a URL to the champion's rune page
-    // Will require string manipulation/interpolation
     let url = "https://u.gg/lol/champions/";
-    return (url = url.concat("", champion));
+    return url.concat("", champion);
 }
 
 export default runesCommand;
