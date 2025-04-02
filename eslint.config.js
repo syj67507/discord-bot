888d9447c601891e77dd7b9d6c9a0b39641ff2d7
Shams Jamal <sj185213@mac-c02cg342md6.lan>
2025-04-01 20:51:26 -0400

    prettier

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+);
